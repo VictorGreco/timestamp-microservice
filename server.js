93d8e53039894c76078d6122469d669df7cc3074
VictorGreco <victorgreco263@gmail.com>
2021-09-13 19:25:16 +0200

    feature: create timestamp-microservice

--- server.js
+++ server.js
@@ -24,6 +24,20 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/:date?', (req, res) => {
+    const { date } = req.params;
+    const getResponse = dateObject => ({ unix: dateObject.getTime(), utc: dateObject.toUTCString() });
+    
+    if (date === undefined) res.json(getResponse(new Date()));
+
+    const parseDate = typeof +date === 'number' && !isNaN(+date) ? +date : date;
+    const isValidDate = new Date(parseDate) !== "Invalid Date" && !isNaN(new Date(parseDate));
+
+    if (!isValidDate) res.json({error: "Invalid Date"});
+
+    res.json(getResponse(new Date(parseDate)));
+})
+
 
 
 // listen for requests :)
